de416fa1fccf70f9d81cce861b1ad7f0dbc5e684
Uros Duric <urosduric@Uross-Air.fritz.box>
2025-05-23 09:17:37 +0200

    Add all necessary files for production: templates, database, data files, and scripts

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
-login_manager.login_message = 'Please log in to access this page.'
+login_manager.login_view = 'register'
+login_manager.login_message = 'Please register or log in to access this page.'
 login_manager.login_message_category = 'info'
 
 @login_manager.user_loader
